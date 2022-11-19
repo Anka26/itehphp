fb951b4566dbdf784f56bab215e320a8cc55f2b5
Anka26 <ankateofilovic26@gmail.com>
2022-11-19 16:11:55 +0100

    header-and-footer

---
+++ templates/header.php
@@ -0,0 +1,65 @@
+<?php
+
+?>
+
+<head>
+    <title>Turistic Attractions</title>
+    <link rel="icon" type="image/x-icon" href="../img/icon.png">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style>
+        .title-text {
+            font-size: xx-large;
+        }
+
+        .nav-text {
+            font-size: large;
+            font-weight: 600;
+        }
+
+        .icon-card {
+            max-height: 100px;
+            margin: 40px auto -30px;
+            display: block;
+            position: relative;
+            top: -30px;
+        }
+
+        .radius-card {
+            border-radius: 20px;
+        }
+
+        .form {
+            padding: 10px;
+            margin-left: 25%;
+            width: 50%;
+            text-align: center;
+            border-radius: 15px;
+        }
+
+        .writer {
+            margin-left: 45%;
+            max-width: 10%;
+            height: auto;
+        }
+    </style>
+</head>
+
+<body class="deep-orange lighten-4">
+    <nav class="deep-orange darken-3">
+        <div class="container">
+            <a href="index.php" class="title-text">TuristicAttractions</a>
+            <ul class="right hide-on-small-and-down navul">
+                <li>
+                    <a href="attractions.php" class="btn white deep-orange-text nav-text z-depth-0">
+                        All Attractions
+                    </a>
+                </li>
+                <li>
+                    <a href="add.php" class="btn white deep-orange-text nav-text z-depth-0">
+                        Add An Attraction
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
